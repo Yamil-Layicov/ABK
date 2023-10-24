36f23cf3652cdff2240999c7e7f2f4a5fdc0d01d
Yamil <yamilbl@code.edu.az>
2023-10-24 16:33:09 +0400

    footer

--- abk/src/components/footer/Footer.jsx
+++ abk/src/components/footer/Footer.jsx
@@ -1,7 +1,53 @@
+import './footer.scss'
+import fb from '../../assets/socials/facebook_icon.svg';
+import insta from '../../assets/socials/instagram_icon.svg';
+import twt from '../../assets/socials/twitter_icon.svg';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer">
+      <div className="boxes">
+        <div className="firstBox">
+          <img src="https://bioxlab-next-js.vercel.app/assets/img/logo/white-logo.png" alt="" />
+          <p>Exerci tation ullamcorper suscipit lobortis nisl aliquip ex ea commodo claritatem itamconse quat.Exerci tation ullamcorper.</p>
+          <div className="socials">
+            <img src={fb} alt="" />
+            <img src={insta} alt="" />
+            <img src={twt} alt="" />
+          </div>
+        </div>
+        <div className="secondBox">
+          <h4>Useful links</h4>
+          <p>Contact us</p>
+          <p>Help & About us</p>
+          <p>Shipping & Returns</p>
+          <p>Refund Policy</p>
+          <p>About us</p>
+          <p>Services</p>
+        </div>
+        <div className="thirdBox">
+          <h4>Contact info</h4>
+          <p>Ta-134/A, Gulshan Badda Link</p>
+          <p>(+880)155 69569 365</p>
+          <p>support@rstheme.com</p>
+          <p>Office Hours: 8AM - 11PM</p>
+          <p>Sunday - Wekend Day</p>
+        </div>
+        <div className="subcriber">
+          <h4>Subscribe Newslatter</h4>
+          <p>Exerci tation ullamcorper suscipit lobortis nisl aliquip ex ea commodo</p>
+          <div className="inputMsg"><input type="text" placeholder='enter email'/></div>
+        </div>
+      </div>
+      <div className="copyright">
+        <span>© Copyright © 2023 Theme_pure. All Rights Reserved Copyright</span>
+        <div>
+          <span>Terms and conditions</span>
+          <span>Privacy policy</span>
+          <span>Pricing</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
