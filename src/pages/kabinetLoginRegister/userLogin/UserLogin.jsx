8a3f5722c99eb9b88e6ba489568079cbbe47c283
Yamil <yamilbl@code.edu.az>
2023-12-19 17:33:48 +0400

    added eye

--- src/pages/kabinetLoginRegister/userLogin/UserLogin.jsx
+++ src/pages/kabinetLoginRegister/userLogin/UserLogin.jsx
@@ -4,10 +4,15 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { DevTool } from "@hookform/devtools";
 import {  useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { FaEye } from "react-icons/fa";
+import { FaEyeSlash } from "react-icons/fa";
+
+
 
 const schema = yup.object().shape({
   email: yup.string().email().required(),
-  password: yup
+  password: yup 
     .string()
     .required()
     .min(8, "Password must be at least 8 characters"),
@@ -18,7 +23,11 @@ const FormValues = {
   password: "",
 };
 
+
 const UserLogin = () => {
+
+  const navigate = useNavigate();
+
   const { register, control, handleSubmit, formState } = useForm({
     defaultValues: FormValues,
     resolver: yupResolver(schema),
@@ -46,7 +55,7 @@ const UserLogin = () => {
               type="email"
               placeholder="E-mail ünvan"
               {...register("email")}
-              style={{ border: `1px solid ${errors.email ? "red" : ""}` }}
+              style={{ border: `1px solid ${errors.email ? "red" : "#9A9696"}` }}
             />
 
             <div style={{ position: "relative", width: "100%" }}>
@@ -54,23 +63,20 @@ const UserLogin = () => {
                type={showPassword ? "text" : "password"}
                 placeholder="Şifrə"
                 {...register("password")}
-                style={{ border: `1px solid ${errors.password ? "red" : ""}` , width: "100%" }}
+                style={{ border: `1px solid ${errors.password ? "red" : "#9A9696"}` , width: "100%" }}
               />
               <div
               onClick={handleTogglePassword}
                 style={{
                   position: "absolute",
-                  top: "15px",
-                  right  :"10px",
-                  width: "30px",
-                  height: "30px",
+                  top: "12px",
+                  right  :"20px",
+                  color:"gray",
+                  fontSize:"22px",
+                  cursor:"pointer"
                 }}
               >
-                <img
-                  src="/hide_outline_24.png"
-                  alt=""
-                  style={{ width: "100%" }}
-                />
+                {showPassword ? <FaEye /> : <FaEyeSlash />}
               </div>
             </div>
 
@@ -78,7 +84,7 @@ const UserLogin = () => {
           </form>
           <DevTool control={control} />
           <p className="accaountText">
-            Hesabınız yoxdur? <span>Qeydiyyatdan keç</span>
+            Hesabınız yoxdur? <span onClick={() => navigate("/account/register")}>Qeydiyyatdan keç</span>
           </p>
         </div>
       </div>
--- src/pages/kabinetLoginRegister/userRegister/UserRegister.jsx
+++ src/pages/kabinetLoginRegister/userRegister/UserRegister.jsx
@@ -1,8 +1,9 @@
 import "./style.scss";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import { DevTool } from "@hookform/devtools";
+import { useNavigate } from "react-router-dom";
 
 const schema = yup.object().shape({
   username: yup.string().required(),
@@ -18,20 +19,44 @@ const schema = yup.object().shape({
     .required(),
 });
 
+
 const FormValues = {
   username: "",
   email: "",
   company: "",
   password: "",
   confirmPassword: "",
+  phNumbers: [{ numer: "" }],
 };
 
 const UserRegister = () => {
-  const { register, control, handleSubmit, formState } = useForm({
+
+  const navigate = useNavigate();
+
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState,
+    watch,
+    setValue,
+    disabled,
+  } = useForm({
     defaultValues: FormValues,
     resolver: yupResolver(schema),
   });
-  const { errors } = formState;
+
+  const { errors, touchedFields, dirtyFields, isDirty, isValid, isSubmitting, isSubmitted, isSubmitSuccessful } =
+    formState;
+
+  // const {fields, append, remove} = useFieldArray({
+  //   name:"phNumbers",
+  //   control
+  // })
+
+  console.log(touchedFields, dirtyFields, isDirty);
+
+  const watchUsername = watch("username");
 
   const onSubmit = (data) => {
     console.log(data);
@@ -45,41 +70,72 @@ const UserRegister = () => {
           <h3>Qeydiyyat</h3>
           <form onSubmit={handleSubmit(onSubmit)}>
             <input
-              style={{ border: `1px solid ${errors.username ? "red" : ""}` }}
+              style={{
+                border: `1px solid ${errors.username ? "red" : "#9A9696"}`,
+              }}
               type="text"
               placeholder="Ad və soyad"
               {...register("username", { required: "bos qoyma" })}
             />
-            {/* <p>{errors.username?.message}</p> */}
+            {/* <span>{errors.username?.message}</span> */}
             <input
               type="email"
               placeholder="E-mail ünvan"
               {...register("email")}
-              style={{ border: `1px solid ${errors.email ? "red" : ""}` }}
+              style={{
+                border: `1px solid ${errors.email ? "red" : "#9A9696"}`,
+              }}
             />
             <input
               type="text"
               placeholder="Qurum adı"
               {...register("company")}
-              style={{ border: `1px solid ${errors.company ? "red" : ""}` }}
+              style={{
+                border: `1px solid ${errors.company ? "red" : "#9A9696"}`,
+              }}
             />
             <input
               type="password"
               placeholder="Şifrə"
               {...register("password")}
-              style={{ border: `1px solid ${errors.password ? "red" : ""}` }}
+              style={{
+                border: `1px solid ${errors.password ? "red" : "#9A9696"}`,
+              }}
             />
             <input
               type="password"
               placeholder="Şifrəni təkrar daxil edin"
               {...register("confirmPassword")}
-              style={{ border: `1px solid ${errors.confirmPassword ? "red" : ""}` }}
+              style={{
+                border: `1px solid ${
+                  errors.confirmPassword ? "red" : "#9A9696"
+                }`,
+              }}
             />
-            <button type="submit">Qeydiyyatdan keç</button>
+
+            {/* <div>
+              {fields.map((field, index) => 
+                <>
+                 <input
+                key={field.id}
+                placeholder="numbers"
+                {...register(`phNumbers.${index}.number`)}
+                
+              />
+              <button type="button" onClick={() => remove(index)}>Remove phone number</button>
+                </>
+             )}
+            
+             <button type="button" onClick={() => append({number: ""})}>Add phone number</button>
+            </div> */}
+
+            <button  type="submit">
+              Qeydiyyatdan keç
+            </button>
           </form>
           <DevTool control={control} />
           <p className="accaountText">
-            Hesabınız var? <span>Daxil ol</span>
+            Hesabınız var? <span onClick={() => navigate("/account")}>Daxil ol</span>
           </p>
         </div>
       </div>
