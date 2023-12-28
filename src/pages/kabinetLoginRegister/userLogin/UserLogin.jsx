1e3f1f31105e4dc12e7949f81ee9403ef9700da1
Yamil <yamilbl@code.edu.az>
2023-12-28 17:41:29 +0400

    addeda1

--- src/pages/kabinetLoginRegister/userLogin/UserLogin.jsx
+++ src/pages/kabinetLoginRegister/userLogin/UserLogin.jsx
@@ -13,11 +13,8 @@ const basicSchema = yup.object().shape({
     .string()
     .email("etibarlı e-poçt ünvanını daxil edin")
     .required("e-poçt qeyd olunmalıdır"),
-    password: yup
-    .string()
-    .required("şifrə qeyd olunmalıdır"),
+  password: yup.string().required("şifrə qeyd olunmalıdır"),
 });
-
 
 const initialValues = {
   email: "",
@@ -44,7 +41,7 @@ const UserLogin = () => {
     try {
       console.log(values);
       toast.success("Uğurlu giriş");
-      navigate("/userAccount")
+      navigate("/userAccount");
       actions.resetForm({ values: initialValues });
     } catch (error) {
       console.error(error);
@@ -74,7 +71,7 @@ const UserLogin = () => {
         <div className="intoRegister">
           <h3>Şəxsi kabinetə giriş</h3>
           <form noValidate ref={form} onSubmit={handleSubmit}>
-          <div className="inputBox">
+            <div className="inputBox">
               <input
                 type="email"
                 id="email"
@@ -82,15 +79,11 @@ const UserLogin = () => {
                 value={values.email}
                 onChange={handleChange}
                 onBlur={handleBlur}
-                className={
-                  errors.email && touched.email ? "inputError" : ""
-                }
+                className={errors.email && touched.email ? "inputError" : ""}
               />
-              {errors.email && touched.email && (
-                  <small>{errors.email}</small>
-                )}
+              {errors.email && touched.email && <small>{errors.email}</small>}
             </div>
-            <div className="inputBox" >
+            <div className="inputBox">
               <input
                 type={showPassword ? "text" : "password"}
                 placeholder="Şifrə"
@@ -101,42 +94,49 @@ const UserLogin = () => {
                 className={
                   errors.password && touched.password ? "inputError" : ""
                 }
-                />
-                {errors.password && touched.password && (
-                  <small>{errors.password}</small>
-                )}
-              <div
-              className="inputEyes"
-                onClick={handleTogglePassword} >
+              />
+              {errors.password && touched.password && (
+                <small>{errors.password}</small>
+              )}
+              <div className="inputEyes" onClick={handleTogglePassword}>
                 {showPassword ? <FaEye /> : <FaEyeSlash />}
               </div>
             </div>
             <div className="forgotBox">
               <div className="saveBox" onClick={() => handleButtonClick()}>
-                <span className="checkMark" >
-                  {isChecked ? <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="14"
-                    height="14"
-                    viewBox="0 0 18 14"
-                    fill="none"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M17.7364 0.663597C18.0878 1.01507 18.0878 1.58492 17.7364 1.93639L6.6364 13.0364C6.46761 13.2052 6.2387 13.3 6 13.3C5.76131 13.3 5.53239 13.2052 5.3636 13.0364L0.263604 7.93639C-0.0878682 7.58492 -0.0878678 7.01507 0.263604 6.6636C0.615076 6.31213 1.18492 6.31213 1.5364 6.6636L6 11.1272L16.4636 0.663598C16.8151 0.312126 17.3849 0.312126 17.7364 0.663597Z"
-                      fill="#9A9696"
-                    />
-                  </svg> : ""}
+                <span className="checkMark">
+                  {isChecked ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M17.2803 4.71967C17.5732 5.01256 17.5732 5.48744 17.2803 5.78033L8.03033 15.0303C7.88968 15.171 7.69891 15.25 7.5 15.25C7.30109 15.25 7.11032 15.171 6.96967 15.0303L2.71967 10.7803C2.42678 10.4874 2.42678 10.0126 2.71967 9.71967C3.01256 9.42678 3.48744 9.42678 3.78033 9.71967L7.5 13.4393L16.2196 4.71967C16.5125 4.42678 16.9874 4.42678 17.2803 4.71967Z"
+                        fill="#231781"
+                      />
+                    </svg>
+                  ) : (
+                    ""
+                  )}
                 </span>
                 <span className="textSave">Yadda saxla</span>
               </div>
-              <span className="text" onClick={() => navigate("/account/updatePassword")}>Şifrəni unutdun?</span>
+              <span
+                className="text"
+                onClick={() => navigate("/account/updatePassword")}
+              >
+                Şifrəni unutdun?
+              </span>
             </div>
             <button type="submit">Daxil ol</button>
           </form>
           <p className="accaountText">
-            Hesabınız yoxdur? 
+            Hesabınız yoxdur?
             <span onClick={() => navigate("/account/register")}>
               Qeydiyyatdan keç
             </span>
